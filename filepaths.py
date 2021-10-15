aed92b286e9844a586f50e9e34b9bafb7e4f16a6
Yash Agarwal <yashag66@gmail.com>
2021-10-15 15:37:01 +0530

    Added Ipython Notebook for BCG Case Study

---
+++ filepaths.py
@@ -0,0 +1,4 @@
+PRIMARY_PERSON_DATA_PATH = './Data/Primary_Person_use.csv'
+UNITS_DATA_PATH = './Data/Units_use.csv'
+CHARGES_DATA_PATH = './Data/Charges_use.csv'
+DAMAGES_DATA_PATH = './Data/Damages_use.csv'
